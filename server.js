46e806bf4ecf9845e35d0d7d31f9b960ca32e13a
simonkcoding <ykwok3@uncc.edu>
2018-11-17 10:20:52 -0500

    123

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 app.use('/api/articles',articles);
 if (process.env.NODE_ENV ==='production'){
     app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 //Connect mongo DB
